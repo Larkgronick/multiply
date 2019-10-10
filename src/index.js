2b7ad002ae727c89d19369909e6bbfeeac88aac9
Nikita Zhavoronkov <larkgronick@gmail.com>
2019-10-10 15:52:32 +0300

    feat: complete multiply task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+
+    var reverseFirst = first.split('').reverse();
+    var reverseSecond = second.split('').reverse();
+  
+    var resultMultiply = [];
+  
+    for (var i = 0; i < reverseFirst.length; i++) {
+      for (var j = 0; j < reverseSecond.length; j++) {
+        var m = reverseFirst[i] * reverseSecond[j];
+        resultMultiply[i + j] = (resultMultiply[i + j]) ? resultMultiply[i + j] + m : m;
+      }
+    }
+  
+    for (var i = 0; i < resultMultiply.length; i++) {
+      var num = resultMultiply[i] % 10;
+      var move = Math.floor(resultMultiply[i] / 10);
+      resultMultiply[i] = num;
+  
+      if (resultMultiply[i + 1])
+        resultMultiply[i + 1] += move;
+      else if (move != 0)
+        resultMultiply[i + 1] = move;
+    }
+  
+    return resultMultiply.reverse().join('');
+  }
